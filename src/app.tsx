dd2a42990d755bcaa65ad38ba0af5d4a6cea754c
Bojan Raic <bojanraic@users.noreply.github.com>
2023-11-11 20:37:31 +0100

    Minor Tweaks
    - bumped devDependencies versions
    - `npm audit fix`-ed vulnerable dependencies
    - changed private session indicator CSS selector
    - cleaned up init types and private session condition

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 const MAIN_MENU_SELECTOR: string = "button.main-userWidget-boxCondensed";
-const PRIVATE_SESSION_INDICATOR_SELECTOR: string = "button.main-noConnection-isNotice";
+const PRIVATE_SESSION_INDICATOR_SELECTOR: string = "button.main-noConnection-button";
 const PRIVATE_SESSION_MENU_ITEM_SELECTOR: string = "ul.main-contextMenu-menu > li:nth-last-child(3) > button.main-contextMenu-menuItemButton";
 
 (async () => {
-    const init: any = (condition: any, callback: any) => condition() ? callback() : setTimeout(() => init(condition, callback), 250);
+    const init: (condition: () => boolean, callback: () => void) => void = (condition, callback) => condition() ? callback() : setTimeout(() => init(condition, callback), 250);
     init(() => Spicetify.Platform, () => {
-        if (!document.querySelector(PRIVATE_SESSION_INDICATOR_SELECTOR) as Boolean) {
+        if (!(document.querySelector(PRIVATE_SESSION_INDICATOR_SELECTOR) as HTMLButtonElement)) {
             (document.querySelector(MAIN_MENU_SELECTOR) as HTMLButtonElement)?.click();
             (document.querySelector(PRIVATE_SESSION_MENU_ITEM_SELECTOR) as HTMLButtonElement)?.click();
         }
